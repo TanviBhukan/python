d517014ecd5d9347692e1f1cee5d4838fb463e27
TanviBhukan <157092275+TanviBhukan@users.noreply.github.com>
2024-09-26 07:34:20 +0530

    Compute element-wise sum of tuples

---
+++ tuplista.py
@@ -0,0 +1,25 @@
+# Using tuples
+a = (1, 2, 3, 4)
+b = (4, 5, 6, 7)
+c = (2, 2, 3, 8)
+
+print("Original tuples:")
+print(a)
+print(b)
+print(c)
+
+print("\nElement-wise sum of the said tuples:")
+result = tuple(map(sum, zip(a, b, c)))
+print(result)
+
+# Using NumPy arrays
+from numpy import array
+
+list1 = [1, 5, 7]
+list2 = [3, 2, 1]
+
+y = array(liSst1)
+z = array(list2)
+
+print("\nElement-wise sum of the said lists (as arrays):")
+print(y + z)
