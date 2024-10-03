520d00fd79b07bee0f805be423c7b7bde0d53b66
TanviBhukan <157092275+TanviBhukan@users.noreply.github.com>
2024-10-03 10:11:07 +0530

    GUI program to create background with changing colors

---
+++ bgchangebygui.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+# Create the main window with a title
+gui = Tk(className='Python Window Color')
+
+# Set window size
+gui.geometry("400x200")
+
+# Set window color
+gui.configure(bg='pink')
+
+# Run the application
+gui.mainloop()
